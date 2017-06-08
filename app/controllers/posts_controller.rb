259e606c7e2501678f9a2ee605f50628c9ae22bc
Akshay Pilania <theakshaypilania@gmail.com>
2017-06-09 02:54:45 +0530

    delete and edit action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,11 @@ def update
 	end
 end
 
+def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+	redirect_to posts_path
+end
 private
 	def post_params
 		params.require(:post).permit(:image, :caption)
