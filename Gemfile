64e9da72f3ea58274a9933f2ff19f6aa7e9c52df
Akshay Pilania <theakshaypilania@gmail.com>
2017-06-09 02:33:56 +0530

    new layouts

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'simple_form'
 gem "paperclip", "~> 5.0.0"
 gem 'haml'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
+gem 'popper_js'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.3'
 # Use sqlite3 as the database for Active Record
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,10 @@ def index
 	@posts = Post.all
 end
 
+def show
+	@post = Post.find(params[:id])
+end
+
 def new
 	@post = Post.new
 end
@@ -15,6 +19,20 @@ def create
 		redirect_to posts_path
 	else
 		render 'new'
+	end
+end
+
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	@post = Post.find(params[:id])
+	@post.update
+	if @post.update
+		redirect_to @post
+	else
+		render 'edit'
 	end
 end
 
