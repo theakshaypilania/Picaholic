2382ff50c37e1d8508f632038a51d8149e20f15f
Akshay Pilania <theakshaypilania@gmail.com>
2017-07-13 03:03:07 +0530

    added some AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,18 +6,26 @@ def create
 	@comment.user_id = current_user.id
 
 	if @comment.save
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form, something went wrong."
       render root_path
-    end
+  end
 end
 
 def destroy
-	@comment = @post.comments.find(params[:id])
-	@comment.destroy
-	flash[:success] = "Comment deleted"
-    redirect_to root_path
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
 end
 
 private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
    	#validates :image, presence: true
     validates :user_id, presence: true  
- 	validates :caption, presence: true
+ 	validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
     belongs_to :user  
     has_many :comments, dependent: :destroy
 	has_attached_file :image, styles: {:medium => "640x"}
